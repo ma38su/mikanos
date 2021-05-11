9c2f91def0f06713e76b4ba798a2a4f7b6e8d00b
ma38su <ma38su@gmail.com>
2021-05-11 09:54:37 +0900

    day05c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -41,7 +41,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
   WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+  WriteAscii(*pixel_writer, 58, 50, 'B', {0, 0, 0});
   while (1) __asm__("hlt");
 }
 
