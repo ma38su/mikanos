25d3f1c75a5952bc1198b1db486bf09f8d2b0cb6
ma38su <ma38su@gmail.com>
2021-05-11 12:25:12 +0900

    day05d

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <cstddef>
+#include <cstdio>
 
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
@@ -40,8 +41,17 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
-  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-  WriteAscii(*pixel_writer, 58, 50, 'B', {0, 0, 0});
+
+  int i = 0;
+  for (char c = '!'; c <= '~'; ++c, ++i) {
+    WriteAscii(*pixel_writer, i * 8, 50, c, {0, 0, 0});
+  }
+  WriteString(*pixel_writer, 0, 66, "Hello, World!", {0, 0, 255});
+
+  char buf[128];
+  sprintf(buf, "1 + 2 = %d", 1 + 2);
+
+  WriteString(*pixel_writer, 0, 82, buf, {0, 0, 255});
   while (1) __asm__("hlt");
 }
 
