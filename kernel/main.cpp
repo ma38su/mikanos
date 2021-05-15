04aaeb534add7fe45fe1a07ff65134f6ba6f7bad
ma38su <ma38su@gmail.com>
2021-05-16 01:23:36 +0900

    tmp

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -20,6 +20,7 @@
 #include "queue.hpp"
 #include "segment.hpp"
 #include "paging.hpp"
+#include "memory_manager.hpp"
 
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
@@ -42,6 +43,32 @@ int printk(const char* format, ...) {
   console->PutString(s);
   return result;
 }
+
+::memory_manager = new(memory_manager_buf) BitmapMemoryManager;
+
+const auto memory_map_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
+uintptr_t available_end = 0;
+for (uintptr_t iter = memory_map_base;
+     iter < memory_map_base + memory_map.map_size;
+     iter += memory_map.descriptor_size) {
+  auto desc = reinterpret_cast<const MemoryDescriptor*>(iter);
+  if (available_end < desc->physical_start) {
+    memory_manager->MarkAllocated(
+        FrameID{available_end / kBytesPerFrames},
+        (desc->physical_start - available_end) / kBytesPerFrame);
+  }
+
+  const auto physical_end =
+    desc->physical_start + desc->number_of_pages * kUEFIPageSize;
+  if (IsAvailable(static_cast<MemoryType>(desc->type))) {
+    available_end = physical_end;
+  } else {
+    memory_manager->MarkAllocated(
+        FrameID{desc->physical_start / kBytesPerFrame},
+        desc->number_of_pages * kUEFIPageSize / kBytePerFrame);
+  }
+}
+memory_manager->SetMemoryRange(FrameID{1}, FrameID{available_end / kBytesPerFrame});
 
 char mouse_cursor_buf[sizeof(MouseCursor)];
 MouseCursor* mouse_cursor;
