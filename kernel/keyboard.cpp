6aac5b11cb0d1a78088815519fe631ba96484c21
ma38su <ma38su@gmail.com>
2021-05-27 14:13:53 +0900

    day14b

--- kernel/keyboard.cpp
+++ kernel/keyboard.cpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include "usb/classdriver/keyboard.hpp"
+#include "task.hpp"
 
 namespace {
 
@@ -55,9 +56,9 @@ const int kRGUIBitMask     = 0b10000000u;
 } // namespace
 
 // #@@range_begin(initkb)
-void InitializeKeyboard(std::deque<Message>& msg_queue) {
+void InitializeKeyboard() {
   usb::HIDKeyboardDriver::default_observer =
-    [&msg_queue](uint8_t modifier, uint8_t keycode) {
+    [](uint8_t modifier, uint8_t keycode) {
       const bool shift = (modifier & (kLShiftBitMask | kRShiftBitMask)) != 0;
       char ascii = keycode_map[keycode];
       if (shift) {
@@ -67,7 +68,7 @@ void InitializeKeyboard(std::deque<Message>& msg_queue) {
       msg.arg.keyboard.modifier = modifier;
       msg.arg.keyboard.keycode = keycode;
       msg.arg.keyboard.ascii = ascii;
-      msg_queue.push_back(msg);
+      task_manager->SendMessage(1, msg);
     };
 }
 // #@@range_end(initkb)
