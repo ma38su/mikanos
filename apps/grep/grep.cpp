b174d00863e511ad16e070069770f459c7fced05
ma38su <ma38su@gmail.com>
2021-07-10 00:16:09 +0900

    day29c

--- apps/grep/grep.cpp
+++ apps/grep/grep.cpp
@@ -3,27 +3,28 @@
 #include <regex>
 
 extern "C" void main(int argc, char** argv) {
-  if (argc < 3) {
+  if (argc < 2) {
     printf("Usage: %s <pattern> <file>\n", argv[0]);
     exit(1);
   }
 
-  std::regex pattern{argv[1]};
-
-  FILE* fp = fopen(argv[2], "r");
-  if (fp == nullptr) {
-    printf("failed to open: %s\n", argv[2]);
-    exit(1);
+  FILE* fp = stdin;
+  if (argc >= 3) {
+    fp = fopen(argv[2], "r");
+    if (fp == nullptr) {
+      fprintf(stderr, "failed to open: %s\n", argv[2]);
+      exit(1);
+    }
   }
 
-  uint32_t no = 0;
+  std::regex pattern{argv[1]};
+
   char line[256];
   while (fgets(line, sizeof(line), fp)) {
     std::cmatch m;
     if (std::regex_search(line, m, pattern)) {
-      printf("%d: %s", no, line);
+      printf(line);
     }
-    no++;
   }
   exit(0);
 }
